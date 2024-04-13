ad1231b325ab5fb05e41ca51b812edccf48b3bd3
pondexplorer.eth <pondexplorer@proton.me>
2024-04-13 11:26:14 -0700

    Update TokenFactory.sol

--- TokenFactory.sol
+++ TokenFactory.sol
@@ -1,1 +1,26 @@
- 
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract TokenTemplate is ERC20 {
+    constructor(string memory name, string memory symbol, uint256 initialSupply, address creator) ERC20(name, symbol) {
+        _mint(creator, initialSupply);
+    }
+}
+
+contract TokenFactory {
+    mapping(address => address[]) public userTokens;
+
+    event TokenCreated(address tokenAddress, string tokenName, string tokenSymbol);
+
+    function createToken(string memory name, string memory symbol, uint256 initialSupply) public {
+        TokenTemplate newToken = new TokenTemplate(name, symbol, initialSupply, msg.sender);
+        userTokens[msg.sender].push(address(newToken));
+        emit TokenCreated(address(newToken), name, symbol);
+    }
+
+    function getUserTokens(address user) public view returns (address[] memory) {
+        return userTokens[user];
+    }
+}
